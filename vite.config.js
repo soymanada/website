232600afedc9289acd78d065c1d1ea40242d6315
Francisco Aleuy <faleuy@gmail.com>
2026-03-17 19:54:15 -0300

    Fix: Final absolute paths for soymanada.com

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Al usar soymanada.com, la base debe ser la raíz '/'
-  base: '/',
+  base: '/', // ESTO ES LO MÁS IMPORTANTE
 })
